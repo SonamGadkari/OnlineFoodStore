d8525aa44cbeb6bb82ee06f14dca00e4e17fe52d
Sonam Gadekari <S533490@nwmissouri.edu>
2019-08-14 17:17:00 -0500

    Created Customer model and views

--- src/main/java/sonam/com/OnlineFoodStore/controller/HomeController.java
+++ src/main/java/sonam/com/OnlineFoodStore/controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sonam.com.OnlineFoodStore.dao.CartDao;
+import sonam.com.OnlineFoodStore.dao.CustomerDao;
+import sonam.com.OnlineFoodStore.dao.LoginDao;
 import sonam.com.OnlineFoodStore.dao.ProductDao;
 import sonam.com.OnlineFoodStore.model.Product;
 
@@ -13,7 +16,11 @@ import java.util.List;
 @Controller
 public class HomeController {
     private ProductDao productDao=new ProductDao();
-   
+    private CustomerDao customerDao =new CustomerDao();
+    private CartDao cartDao =new CartDao();
+    private LoginDao loginDao =new LoginDao();
+    private LoginDao loginDao1 =new LoginDao();
+    
     @RequestMapping("/")
     public String home(Model model) {
         List<Product> productList=productDao.getProductList();
@@ -29,7 +36,54 @@ public class HomeController {
         model.addAttribute(product);
         return "viewProduct";
     }
- 
+
+    @RequestMapping("/viewCustomer")
+    public String viewCustomer(Model model)
+    {
+        String customer=customerDao.getCustomerName();
+        model.addAttribute(customer);
+        return "viewCustomer";
+    }
+
+    @RequestMapping("/viewCardDetails")
+    public String viewCardDetails(Model model)
+    {
+        String customer=customerDao.getCustomerName();
+        model.addAttribute(customer);
+        return "viewCardDetails";
+    }
+
+    @RequestMapping("/viewCart")
+    public String viewCart(Model model)
+    {
+        String cart=cartDao.getCartList().toString();
+        model.addAttribute(cart);
+        return "viewCart";
+    }
+
+    @RequestMapping("/viewLogin")
+    public String viewLogin(Model model)
+    {
+        String login=loginDao.getUserName();
+        model.addAttribute(login);
+        return "viewLogin";
+    }
+    
+    @RequestMapping("/viewAdminLogin")
+    public String viewAdminLogin(Model model)
+    {
+        String login=loginDao.getUserName();
+        model.addAttribute(login);
+        return "viewAdminLogin";
+    }
+
+    @RequestMapping("/viewCRUD")
+    public String viewCRUD(Model model)
+    {
+        String login=loginDao.getUserName();
+        model.addAttribute(login);
+        return "viewCRUD";
+    }
 
     @RequestMapping("/home_logout")
     public String homeLogout(Model model) {
@@ -54,7 +108,5 @@ public class HomeController {
         model.addAttribute("products",productList);
         return "viewSignup";
     }
-
-
 
 }
